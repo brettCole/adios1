707d13ee5fc90f8da309159545bccc62f3c41b64
brettCole <acodercalledcole@gmail.com>
2017-12-21 12:44:41 -0500

    fixed misspellings

--- adios/src/reducers/loginStatus.js
+++ adios/src/reducers/loginStatus.js
@@ -6,29 +6,29 @@ export default function loginStatus(state = {
   isLoggedIn: false,
   loading: false
 }, action) {
-  switch(action.type) {
-    case 'RETURN_JWT':
-      return { loading: false }
-    case 'CURENT_USER':
-      const user = Object.assign({}, state, action.payload);
-      return {
-        id: user.id,
-        username: user.username,
-        name: user.name,
-        email: user.email,
-        isLoggedIn: true,
-        loading: false
-      }
-    case 'LOGGED_OUT':
-      return {
-        loading: false,
-        isLoggedIn: false,
-        id: null,
-        username: null,
-        name: null,
-        email: null
-      }
-    default:
-      return state;
-  }
+switch(action.type) {
+  case 'RETURN_JWT':
+    return { loading: false }
+  case 'CURRENT_USER':
+    const user = Object.assign({}, state, action.payload);
+    return {
+      id: user.id,
+      username: user.username,
+      name: user.name,
+      email: user.email,
+      isLoggedIn: true,
+      loading: false
+    }
+  case 'LOGGED_OUT':
+    return { 
+      loading: false, 
+      isLoggedIn: false,
+      id: null,
+      username: null,
+      name: null,
+      email: null
+    }
+  default:
+    return state;
 }
+} 
