79d0553b9176d022fc875e70c20562740fc82c01
brettCole <acodercalledcole@gmail.com>
2017-12-20 22:38:31 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'user_token' => 'user_token#create'
+  post '/homeaway_connect' => 'searches#homeaway_connect'
+
+  namespace :api do
+    namespace :v1 do
+      post '/register' => 'users#create'
+      resources :users
+      post '/create_checklist' => 'checklists#create'
+      get '/checklists' => 'checklists#index'
+      get '/checklist' => 'checklists#show'
+      post '/create_item' => 'items#create'
+      post '/email' => 'emails#create'
+      
+      mount Knock::Engine => '/knock'
+    end
+  end 
 end
