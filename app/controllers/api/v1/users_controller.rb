6d5b8e1883501ed625bbaa4c376636e181b2d049
brettCole <acodercalledcole@gmail.com>
2017-12-20 22:09:59 -0500

    user migration/model/controller creation

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user, only: [:show]
+
+  def create
+    user = User.new(user_params)
+    if user.valid? && user.save
+      token = Knock::AuthToken.new(payload: { sub: user.id })
+      render json: token, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: 500
+    end
+  end
+
+  def show
+    render json: current_user, only: [:id, :username, :name]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :checklists
+
+  validates :name, :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+end
