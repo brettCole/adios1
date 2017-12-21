146992b0b1d68258098d0827a4f11d6d35ef3f26
brettCole <acodercalledcole@gmail.com>
2017-12-21 11:58:36 -0500

    react router routes and redux store

--- adios/src/App.js
+++ adios/src/App.js
@@ -1,19 +1,25 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import SignupForm from './components/form/SignupForm';
+import Homepage from './containers/Homepage'
+import LoginForm from './components/form/LoginForm'
+import SearchResultsPage from './containers/SearchResultsPage'
+import ChecklistContainer from './containers/ChecklistContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+          <div className="App">
+            <Route exact path='/' component={Homepage} />
+            <Route path='/signup' component={SignupForm} />
+            <Route path='/login' component={LoginForm} />
+            <Route path='/checklist' component={ChecklistContainer} />
+            <Route path='/search_results' component={SearchResultsPage} />
+          </div>
+      </Router>
     );
   }
 }
