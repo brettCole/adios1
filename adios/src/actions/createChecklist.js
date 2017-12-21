6da40830bd614bd7002ef225a4a948e9783533fc
brettCole <acodercalledcole@gmail.com>
2017-12-21 08:56:42 -0500

    created checklist actions

---
+++ adios/src/actions/createChecklist.js
@@ -0,0 +1,34 @@
+require ('isomorphic-fetch');
+
+export function receiveChecklists(lists) {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING' });
+    dispatch({
+      type: 'RECEIVE_CHECKLISTS',
+      payload: lists
+    });
+    dispatch({ type: 'NOTLOADING' });
+  }
+}
+
+export function createChecklist(data) {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING' });
+    return fetch('http://localhost:3001/api/v1/create_checklist', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+      },
+      body: data 
+    })
+    .then(response => response.json())
+    .then(data => {
+      dispatch({
+        type: 'CREATE_CHECKLISTS_SUCCESS',
+        payload: data
+      });
+    });
+  }
+}
